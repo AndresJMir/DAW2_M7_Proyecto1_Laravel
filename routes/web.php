276ee00b3c60111f522217510859ab94f5faa548
AndresJMir <anunez1@insjoaquimmir.cat>
2023-10-24 17:06:31 +0200

    Rols d'usuari

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-use Illuminate\Support\Facades\Log;
-
-
-Route::get('/', function () {
-   Log::info('Loading welcome page');
-   return view('welcome');
-});
-
 
 /*
 |--------------------------------------------------------------------------
@@ -21,8 +13,19 @@ Route::get('/', function () {
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-/*
+
 Route::get('/', function () {
     return view('welcome');
 });
-*/
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
